834aae2a0727c12e922710eccec4c887ca921d16
Sixah <sishuai@ranbb.com>
2019-11-22 18:33:56 +0800

    Vue-router

---
+++ day9-20191122/learnvuerouter/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <!-- router-link：
+     该标签是一个vue-router中已经内置的组件，它默认会被渲染成一个<a>标签
+     to属性指定跳转的路由
+     tag属性可以指定渲染的标签
+     replace属性可以禁止页面返回
+     默认使用的是pushState方式改变的url.类似于入栈和出栈操作，可以返回
+     replace属性启用replaceState方式改变url
+     -->
+
+    <!--
+      <router-link to="/home" replace active-class="active">首页</router-link>
+    <router-link to="/about" tag="button" replace active-class="active">关于</router-link>
+     -->
+
+    <!--
+    <router-link to="/home" replace>首页</router-link>
+    <router-link to="/about" tag="button" replace>关于</router-link>
+    -->
+
+    <button @click="homeClick">首页</button>
+    <button @click="aboutClick">关于</button>
+
+
+
+    <h1>你真好看！</h1>
+    <!-- router-view
+     该标签会根据当前的路径，动态渲染出不同的组件
+     -->
+    <router-view></router-view>
+    <h1>Sixah</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    homeClick() {
+      // this.$router.push('/home')
+      this.$router.replace('/home')
+    },
+    aboutClick() {
+      // this.$router.push('/about')
+      this.$router.replace('/about')
+    }
+  }
+}
+</script>
+
+<style>
+  /*.router-link-active {*/
+    /*color: red;*/
+  /*}*/
+
+  .active {
+    color: #f00;
+  }
+</style>
