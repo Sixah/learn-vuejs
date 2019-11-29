98d9a5ccd3ace6181e3d3dbed954a94abe05179c
Sixah <sishuai@ranbb.com>
2019-11-29 18:40:02 +0800

    项目框架初始化

---
+++ day13-20191129/supermall/src/network/request.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import baseURL from '@/config/config.env'
+
+export function request(config) {
+  // 1.创建axios的实例
+  const instance = axios.create({
+    baseURL,
+    timeout: 5000,
+  })
+
+  // 2.axios的拦截器
+  // 2.1 请求拦截
+  instance.interceptors.request.use(config => {
+    return config
+  }, err => {
+
+  })
+
+  // 2.2 响应拦截
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+
+  return instance(config)
+}
---
+++ day13-20191129/supermall/src/views/home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">
+        购物街
+      </div>
+    </nav-bar>
+    <h2>首页</h2>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar'
+  import {getHomeMultidata} from 'network/home';
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    }
+  }
+
+  console.log(getHomeMultidata());
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
